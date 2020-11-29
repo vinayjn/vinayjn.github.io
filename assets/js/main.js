a2bb84e1e22988ff031f72499fb7caaaead02a3f
Vinay Jain <vinay.jn7@gmail.com>
2020-11-29 19:17:35 +0530

    Added dark mode detection and theme

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    prefersDark ? switchTo("dark") : switchTo("light")
+});
+
+function switchTo(theme) {
+    const themeLink = document.querySelector("#theme-link");
+    
+    if (theme == "light") {
+        themeLink.href = "/assets/css/light.css";
+    } else {
+        themeLink.href = "/assets/css/dark.css";
+    }
+}
